7c99f1aa9c2f2d2c2f98f5fb8131bf26d6bf824f
Haku <polina231003@mail.ru>
2023-04-04 23:53:51 +0300

    clients

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/GitHubClient.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/GitHubClient.java
@@ -1,14 +1,14 @@
 package ru.tinkoff.edu.java.scrapper.client;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import ru.tinkoff.edu.java.scrapper.dto.GitHubReposResponse;
 
-@RequiredArgsConstructor
 public class GitHubClient {
 
-    private final WebClient webClient;
+    @Autowired
+    private WebClient webClient;
 
     public Mono<GitHubReposResponse> getRepository(String repository){
         String path = "/repos/" + repository;
--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/StackOverflowClient.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/client/StackOverflowClient.java
@@ -1,14 +1,14 @@
 package ru.tinkoff.edu.java.scrapper.client;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import ru.tinkoff.edu.java.scrapper.dto.ListAnswersResponse;
 
-@RequiredArgsConstructor
 public class StackOverflowClient {
 
-    private final WebClient webClient;
+    @Autowired
+    private WebClient webClient;
 
     public Mono<ListAnswersResponse> getAnswers(String questionId){
         String path = "/2.3/questions/"+ questionId +"/answers?site=stackoverflow";
