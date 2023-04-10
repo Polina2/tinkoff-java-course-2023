a6489daea36c21eed4b0d452d5e926b6b8f75e5a
Haku <polina231003@mail.ru>
2023-04-10 18:47:43 +0300

    poms and exceptions

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/exception_handler/ScrapperExceptionHandler.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/controller/exception_handler/ScrapperExceptionHandler.java
@@ -5,22 +5,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ServerWebInputException;
 import ru.tinkoff.edu.java.scrapper.dto.ApiErrorResponse;
 
 @RestControllerAdvice
 public class ScrapperExceptionHandler {
 
-    @ExceptionHandler(TypeMismatchException.class)
-    protected ResponseEntity<ApiErrorResponse> handleBadRequestError(Exception e){
+    @ExceptionHandler({TypeMismatchException.class, ServerWebInputException.class})
+    public ResponseEntity<ApiErrorResponse> handleBadRequestError(Exception e){
         ApiErrorResponse response = createError(e, HttpStatus.BAD_REQUEST.getReasonPhrase(), HttpStatus.BAD_REQUEST.toString());
         return ResponseEntity.status(400).body(response);
-        //return handleExceptionInternal(e, body, new HttpHeaders(), HttpStatusCode.valueOf(400), request);
+    }
+
+    @ExceptionHandler(WebClientResponseException.class)
+    public ResponseEntity<ApiErrorResponse> handleNotFoundError(Exception e){
+        ApiErrorResponse response = createError(e, HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.toString());
+        return ResponseEntity.status(404).body(response);
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ApiErrorResponse> handleNotFoundError(Exception e){
-        ApiErrorResponse response = createError(e, HttpStatus.NOT_FOUND.getReasonPhrase(), HttpStatus.NOT_FOUND.toString());
-        return ResponseEntity.status(404).body(response);
+    public ResponseEntity<ApiErrorResponse> handleInternalServerError(Exception e){
+        ApiErrorResponse response = createError(
+                e,
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                HttpStatus.INTERNAL_SERVER_ERROR.toString()
+        );
+        return ResponseEntity.status(500).body(response);
     }
 
     private ApiErrorResponse createError(Exception ex, String description, String code){
