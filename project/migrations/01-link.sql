4aea7edb76dea1bf86496825af123251c6e63b48
Haku <polina231003@mail.ru>
2023-04-11 16:52:09 +0300

    tz

--- project/migrations/01-link.sql
+++ project/migrations/01-link.sql
@@ -5,5 +5,5 @@ create table link(
                      link_id bigint generated always as identity
                          (start with 1 increment by 1) primary key,
                      url text not null unique,
-                     last_update timestamptz
+                     last_update timestamp
 );
