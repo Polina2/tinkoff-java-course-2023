ddc31ee7a5dea9e4d79db6c5047b7881148b05e4
Haku <polina231003@mail.ru>
2023-04-11 16:43:29 +0300

    testcontainers

---
+++ project/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/test/IntegrationEnvironment.java
@@ -0,0 +1,61 @@
+package ru.tinkoff.edu.java.scrapper.test;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Testcontainers
+public abstract class IntegrationEnvironment {
+    public static final JdbcDatabaseContainer<?> DB_CONTAINER;
+
+    static {
+        DB_CONTAINER = new PostgreSQLContainer<>("postgres:15")
+                .withDatabaseName("scrapper")
+                .withUsername("postgres")
+                .withPassword("postgres");
+        DB_CONTAINER.start();
+        runMigrations(DB_CONTAINER);
+    }
+
+    private static void runMigrations(JdbcDatabaseContainer<?> container){
+        Path changelogPath = new File(".").toPath().toAbsolutePath()
+                .getParent().getParent().resolve("migrations");
+        try {
+            Connection connection = DriverManager.getConnection(
+                    container.getJdbcUrl(), container.getUsername(), container.getPassword()
+            );
+            Database database = DatabaseFactory.getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase = new Liquibase(
+                    "master.xml", new DirectoryResourceAccessor(changelogPath), database
+            );
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | FileNotFoundException | LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @DynamicPropertySource
+    static void jdbcProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasourse.url", DB_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasourse.username", DB_CONTAINER::getUsername);
+        registry.add("spring.datasourse.password", DB_CONTAINER::getPassword);
+    }
+}
---
+++ project/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/test/JdbcTest.java
@@ -0,0 +1,23 @@
+package ru.tinkoff.edu.java.scrapper.test;
+
+import org.junit.Test;
+import org.testcontainers.shaded.org.hamcrest.MatcherAssert;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
+import ru.tinkoff.edu.java.scrapper.dao.Link;
+
+import java.sql.*;
+
+public class JdbcTest extends IntegrationEnvironment{
+
+    @Test
+    public void addTest() throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_CONTAINER.getJdbcUrl(), DB_CONTAINER.getUsername(), DB_CONTAINER.getPassword());
+        Statement statement = connection.createStatement();
+        Link expected = new Link(1L, "https://github.com/testcontainers/testcontainers-java", null);
+        statement.execute("INSERT INTO link(url) VALUES ('"+expected.url()+"')");
+        ResultSet set = statement.executeQuery("SELECT * FROM link");
+        set.next();
+        Link actual = new Link(set.getLong("link_id"), set.getString("url"), set.getTimestamp("last_update"));
+        MatcherAssert.assertThat(actual, Matchers.equalTo(expected));
+    }
+}
