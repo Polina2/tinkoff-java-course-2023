b94a64aeb8d7505c7a4e4c28cff844d91b2bb1f5
Haku <polina231003@mail.ru>
2023-04-13 18:14:38 +0300

    test

--- project/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/test/JdbcTest.java
+++ project/scrapper/src/test/java/ru/tinkoff/edu/java/scrapper/test/JdbcTest.java
@@ -2,7 +2,6 @@ package ru.tinkoff.edu.java.scrapper.test;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import ru.tinkoff.edu.java.scrapper.dto.Link;
 
 import java.sql.*;
 
@@ -12,11 +11,13 @@ public class JdbcTest extends IntegrationEnvironment{
     public void simpleTest() throws SQLException {
         Connection connection = DriverManager.getConnection(DB_CONTAINER.getJdbcUrl(), DB_CONTAINER.getUsername(), DB_CONTAINER.getPassword());
         Statement statement = connection.createStatement();
-        Link expected = new Link(1L, "https://github.com/testcontainers/testcontainers-java", null);
-        statement.execute("INSERT INTO link(url) VALUES ('"+expected.url()+"')");
-        ResultSet set = statement.executeQuery("SELECT * FROM link");
-        set.next();
-        Link actual = new Link(set.getLong("link_id"), set.getString("url"), set.getTimestamp("last_update"));
-        Assertions.assertEquals(expected, actual);
+        ResultSet result = statement.executeQuery("""
+                SELECT COUNT(*)
+                FROM information_schema.tables
+                WHERE table_name = 'link' OR table_name = 'tg_chat' OR table_name = 'subscription'""");
+        int count = -1;
+        if (result.next())
+            count = result.getInt(1);
+        Assertions.assertEquals(3, count);
     }
 }
